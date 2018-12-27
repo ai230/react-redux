0c2052d6ac3858158ff6986016f496f64d054c90
AiYamamoto <ay36230@gmail.com>
2018-12-27 10:14:21 -0800

    update action

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,6 @@
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case "CRETE_COURSE":
+    case "CREATE_COURSE":
       //   state.push(action.course);
       //   return state;
       // Let's use ES6 spread operator
